d860e123e68bd0d05e874971d284b9afae26d68c
Marius Butz <mbpictures13@gmail.com>
2021-10-08 15:59:23 +0200

    add redirect to event selection page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,18 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '../style/Global.scss';
 import {StepperContainer} from "../components/StepperContainer";
+import {useEffect} from "react";
+import {EVENT_SELECTION_KEY, STEP_URLS} from "../constants/Constants";
+import {useRouter} from "next/router";
 
 export default function Global({ Component, pageProps }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (window.localStorage.getItem(EVENT_SELECTION_KEY) !== null) return;
+        router.push(STEP_URLS[0]).catch(console.log);
+    }, []);
+
     return (
         <StepperContainer>
             <Component {...pageProps} />
