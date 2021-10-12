589813d8d480d869f5bf43d07c610409132325a9
Marius Butz <marius@blingblinggames.com>
2021-10-12 18:49:08 +0200

    fix possible exceptions in address validation

--- src/constants/util.ts
+++ src/constants/util.ts
@@ -2,6 +2,7 @@ import {IAddress} from "./interfaces";
 import zippo from "zippo";
 
 export const validateAddress = (address: IAddress) => {
+    if (!address || !address.firstName || !address.lastName || !address.address || !address.country) return false;
     return address.firstName.length > 3 &&
         address.lastName.length > 3 &&
         address.address.length > 5 &&
