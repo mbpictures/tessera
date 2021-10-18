319170f53f06231c391edc1b3337f833e087c058
Marius Butz <marius@blingblinggames.com>
2021-10-18 16:21:09 +0200

    refactor address validator

--- src/constants/util.ts
+++ src/constants/util.ts
@@ -1,16 +1,19 @@
 import {IAddress} from "./interfaces";
 import zippo from "zippo";
 
+export type AddressValidator = (address: IAddress) => boolean;
+export const addressValidatorMap: Record<string, AddressValidator> = {
+    "FirstName": (address) => address.firstName != null && address.firstName.length > 3,
+    "LastName": address => address.lastName != null && address.lastName.length > 3,
+    "Address": address =>  address.address != null && address.address.length > 5 && hasNumber(address.address),
+    "Zip": address => zippo.validate(address.zip),
+    "City": address => address.city != null && address.city.length > 3,
+    "Country": address => address.country != null,
+    "Region": address => address.region != null || address.country.regions.length == 0
+}
+
 export const validateAddress = (address: IAddress) => {
-    if (!address || !address.firstName || !address.lastName || !address.address || !address.country) return false;
-    return address.firstName.length > 3 &&
-        address.lastName.length > 3 &&
-        address.address.length > 5 &&
-        hasNumber(address.address) &&
-        zippo.validate(address.zip) &&
-        address.city.length > 3 &&
-        address.country != null &&
-        (address.country.regions.length == 0 || address.region != null); // if regions are available in country, it has to be set
+    return Object.values(addressValidatorMap).every((addressValidator) => addressValidator(address));
 };
 
 export const hasNumber = (myString) => {
