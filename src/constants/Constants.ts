5e0b1e84b26bd8859d1dc4c8c31bfc18f7410610
Marius Butz <mbpictures13@gmail.com>
2021-10-20 09:39:30 +0200

    fix typo

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,3 +1,3 @@
 export const EVENT_SELECTION_KEY = "event_selection";
-export const STEPS = ["Select Event", "Choose seat", "Personal In,formation", "Payment", "Checkout Order"];
+export const STEPS = ["Select Event", "Choose seat", "Personal Information", "Payment", "Checkout Order"];
 export const STEP_URLS = ["/", "/seatselection", "/information", "/payment"];
