953d81474a3c7258a3d84c5285d897250ebea8d9
Marius Butz <mbpictures13@gmail.com>
2021-10-20 12:54:19 +0200

    reduce information panel width on larger devices

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -2,7 +2,7 @@ import {Step} from "../components/Step";
 import {
     Card, Stack,
     TextField,
-    Typography
+    Typography, useMediaQuery
 } from "@mui/material";
 import React, {useEffect, useState} from "react";
 import {CheckboxAccordion} from "../components/CheckboxAccordion";
@@ -20,7 +20,7 @@ import {AddressComponent} from "../components/form/AddressComponent";
 import {PostalDeliveryShippingComponent} from "../components/shipping/PostalDeliveryShippingComponent";
 import {BoxOfficeShippingComponent} from "../components/shipping/BoxOfficeShippingComponent";
 import {DownloadShippingComponent} from "../components/shipping/DownloadShippingComponent";
-import {Box} from "@mui/system";
+import {Box, useTheme} from "@mui/system";
 
 const validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -32,6 +32,9 @@ export default function Information({direction}) {
     const dispatch = useAppDispatch();
 
     const [selectedShippingMethod, setSelectedShippingMethod] = useState<ShippingType | null>(selector.shipping?.type ?? null);
+
+    const theme = useTheme();
+    const boxStyling = useMediaQuery(theme.breakpoints.up("md")) ? {width: "60%"} : {width: "100%"};
 
     useEffect(() => {
         const valid = validateEmail(selector.email) &&
@@ -56,8 +59,8 @@ export default function Information({direction}) {
     }, [selectedShippingMethod]);
 
     return (
-        <Step direction={direction} style={{width: "100%", maxHeight: "100%"}}>
-            <Box paddingBottom={1} paddingTop={1}>
+        <Step direction={direction} style={{width: "100%", maxHeight: "100%", display: "flex", justifyContent: "center"}}>
+            <Box style={boxStyling} sx={{py: 2}}>
                 <Card>
                     <Stack padding={1} spacing={1}>
                         <Typography>These address given will be used for invoice.</Typography>
