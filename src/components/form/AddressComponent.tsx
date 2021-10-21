49bb1e625a2493a11ffaac572f344741443e1afd
Marius <mbpictures13@gmail.com>
2021-10-21 15:46:29 +0200

    implement form validation for all address inputs

--- src/components/form/AddressComponent.tsx
+++ src/components/form/AddressComponent.tsx
@@ -3,14 +3,37 @@ import {IAddress} from "../../constants/interfaces";
 import {ZIP} from "./ZIP";
 import {useState} from "react";
 import countryRegionData, {Country, Region} from "country-region-data";
+import {addressValidatorMap} from "../../constants/util";
+
+const validateAddressComponent = (address: IAddress, property: string) => {
+    if (addressValidatorMap[property](address)) return null;
+    if (property === "firstName") return "Please enter your firstname here!";
+    if (property === "lastName") return "Please enter your lastname here!";
+    if (property === "address") return "Please enter street and house number!";
+    if (property === "city") return "Please enter your city!";
+    if (property === "country") return "Select your country!";
+    if (property === "region") return "Select your region!";
+}
 
 export const AddressComponent = ({value, onChange}: {value: IAddress, onChange: (newAddress: IAddress) => unknown}) => {
     const [localZip, setLocalZip] = useState<string>(value.zip ?? "");
+    const [firstNameError, setFirstNameError] = useState<string>(null);
+    const [lastNameError, setLastNameError] = useState<string>(null);
+    const [addressError, setAddressError] = useState<string>(null);
+    const [cityError, setCityError] = useState<string>(null);
+    const [countryError, setCountryError] = useState<string>(null);
+    const [regionError, setRegionError] = useState<string>(null);
 
     const handleUpdate = (property, newValue) => {
         const newAddress: IAddress = Object.assign({}, value);
         newAddress[property] = newValue;
         onChange(newAddress);
+        if (property === "firstName") setFirstNameError(validateAddressComponent(newAddress, property));
+        if (property === "lastName") setLastNameError(validateAddressComponent(newAddress, property));
+        if (property === "address") setAddressError(validateAddressComponent(newAddress, property));
+        if (property === "city") setCityError(validateAddressComponent(newAddress, property));
+        if (property === "country") setCountryError(validateAddressComponent(newAddress, property));
+        if (property === "region") setRegionError(validateAddressComponent(newAddress, property));
     };
 
     const handleChangeZip = (newValue: string, valid: boolean) => {
@@ -37,15 +60,40 @@ export const AddressComponent = ({value, onChange}: {value: IAddress, onChange: 
 
     return (
         <Stack spacing={1}>
-            <TextField label="Firstname" value={value.firstName ?? ""} onChange={event => handleUpdate("firstName", event.target.value)} />
-            <TextField label="Lastname" value={value.lastName ?? ""} onChange={event => handleUpdate("lastName", event.target.value)} />
-            <TextField label="Address" value={value.address ?? ""} onChange={event => handleUpdate("address", event.target.value)} />
+            <TextField
+                label="Firstname"
+                value={value.firstName ?? ""}
+                onChange={event => handleUpdate("firstName", event.target.value)}
+                helperText={firstNameError}
+                error={firstNameError !== null}
+            />
+            <TextField
+                label="Lastname"
+                value={value.lastName ?? ""}
+                onChange={event => handleUpdate("lastName", event.target.value)}
+                helperText={lastNameError}
+                error={lastNameError !== null}
+            />
+            <TextField
+                label="Address"
+                value={value.address ?? ""}
+                onChange={event => handleUpdate("address", event.target.value)}
+                helperText={addressError}
+                error={addressError !== null}
+            />
             <Grid container rowSpacing={1}>
                 <Grid item md={4} xs={12}>
                     <ZIP value={localZip} onChange={handleChangeZip} />
                 </Grid>
                 <Grid item md={8} xs={12}>
-                    <TextField label="City" fullWidth value={value.city ?? ""} onChange={event => handleUpdate("city", event.target.value)} />
+                    <TextField
+                        label="City"
+                        fullWidth
+                        value={value.city ?? ""}
+                        onChange={event => handleUpdate("city", event.target.value)}
+                        helperText={cityError}
+                        error={cityError !== null}
+                    />
                 </Grid>
             </Grid>
             <Grid container rowSpacing={1}>
@@ -58,6 +106,8 @@ export const AddressComponent = ({value, onChange}: {value: IAddress, onChange: 
                         getOptionLabel={(option: Country) => option.countryName}
                         isOptionEqualToValue={(option, value) => option?.countryName === value?.countryName && option?.countryShortCode === value?.countryShortCode}
                         value={value.country}
+                        helperText={countryError}
+                        error={countryError !== null}
                     />
                 </Grid>
                 <Grid item md={6} xs={12}>
@@ -71,6 +121,8 @@ export const AddressComponent = ({value, onChange}: {value: IAddress, onChange: 
                                 getOptionLabel={(option: Region) => option.name}
                                 isOptionEqualToValue={(option, value) => option?.name === value?.name && option?.shortCode === value?.shortCode}
                                 value={value.region}
+                                helperText={regionError}
+                                error={regionError !== null}
                             />
                         )
                     }
--- src/constants/util.ts
+++ src/constants/util.ts
@@ -3,13 +3,13 @@ import zippo from "zippo";
 
 export type AddressValidator = (address: IAddress) => boolean;
 export const addressValidatorMap: Record<string, AddressValidator> = {
-    "FirstName": (address) => address.firstName != null && address.firstName.length > 3,
-    "LastName": address => address.lastName != null && address.lastName.length > 3,
-    "Address": address =>  address.address != null && address.address.length > 5 && hasNumber(address.address),
-    "Zip": address => zippo.validate(address.zip),
-    "City": address => address.city != null && address.city.length > 3,
-    "Country": address => address.country != null,
-    "Region": address => address.region != null || address.country.regions.length == 0
+    "firstName": (address) => address.firstName != null && address.firstName.length > 3,
+    "lastName": address => address.lastName != null && address.lastName.length > 3,
+    "address": address =>  address.address != null && address.address.length > 5 && hasNumber(address.address),
+    "zip": address => zippo.validate(address.zip),
+    "city": address => address.city != null && address.city.length > 3,
+    "country": address => address.country != null,
+    "region": address => address.region != null || address.country.regions.length == 0
 }
 
 export const validateAddress = (address: IAddress) => {
