0b2fe3997c4e5c4e404c2feb11f04a4a3ccb3a67
Marius Butz <mbpictures13@gmail.com>
2021-11-22 16:48:26 +0100

    add checkout page

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,3 +1,3 @@
 export const EVENT_SELECTION_KEY = "event_selection";
 export const STEPS = ["Select Event", "Choose seat", "Personal Information", "Payment", "Checkout Order"];
-export const STEP_URLS = ["/", "/seatselection", "/information", "/payment"];
+export const STEP_URLS = ["/", "/seatselection", "/information", "/payment", "/checkout"];
