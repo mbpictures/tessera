3e678dab23c1a3dc08d1524eab0b95bd891b03a2
Marius Butz <mbpictures13@gmail.com>
2021-11-22 19:08:15 +0100

    remove logging

--- src/pages/api/payment_intent/index.ts
+++ src/pages/api/payment_intent/index.ts
@@ -46,7 +46,6 @@ export default async function handler(
 
             res.status(200).json(payment_intent);
         } catch (err) {
-            console.log(err.message);
             res.status(500).json({ statusCode: 500, message: err.message });
         }
     } else {
