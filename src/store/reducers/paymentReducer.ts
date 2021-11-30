ef01fa1ce309ebae3195f14b8b64915f5fc2f4ca
Marius Butz <mbpictures13@gmail.com>
2021-11-30 23:18:46 +0100

    fix late pay now button feedback

--- src/store/reducers/paymentReducer.ts
+++ src/store/reducers/paymentReducer.ts
@@ -2,7 +2,7 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {RootState} from "../store";
 import {PaymentType} from "../factories/payment/PaymentFactory";
 
-type PaymentStatus = "none" | "initiate" | "processing" | "finished" | "failure";
+type PaymentStatus = "none" | "persist" | "initiate" | "processing" | "finished" | "failure";
 
 interface PaymentState {
     payment: IPayment;
