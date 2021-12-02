2e2d77bc4d7c646e69ec52e1757ef9a5f6c1d853
Marius Butz <mbpictures13@gmail.com>
2021-12-02 11:15:47 +0100

    add option to add email providers

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,15 +1,35 @@
 import nodemailer from "nodemailer";
 
 export const getEmailTransporter = async () => {
+    if (process.env.EMAIL_PROVIDER) {
+        return nodemailer.createTransport({
+            service: process.env.EMAIL_PROVIDER,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+    }
+    if (process.env.EMAIL_HOST) {
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT ?? 587,
+            secure: process.env.EMAIL_SECURE ?? false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+    }
     let testAccount = await nodemailer.createTestAccount();
 
     return nodemailer.createTransport({
-        host: process.env.EMAIL_HOST ?? "smtp.ethereal.email",
-        port: process.env.EMAIL_PORT ?? 587,
-        secure: process.env.EMAIL_SECURE,
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: true,
         auth: {
-            user: process.env.EMAIL_USER ?? testAccount.user,
-            pass: process.env.EMAIL_PASS ?? testAccount.pass
+            user: testAccount.user,
+            pass: testAccount.pass
         }
     });
 }
