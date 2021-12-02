55165ac692c2349b10670ec17725d2ef02f4bc17
Marius Butz <mbpictures13@gmail.com>
2021-12-02 11:27:37 +0100

    delete invoices after successfully sending the email

--- src/lib/send.ts
+++ src/lib/send.ts
@@ -63,6 +63,7 @@ export const send = async (orderId) => {
                 reject(error);
                 return;
             }
+            fs.unlinkSync(invoicePath);
             resolve();
         })
     })
