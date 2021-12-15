dc45e6a48dd355e964d711dcb7192231313778a9
Marius <mbpictures13@gmail.com>
2021-12-15 15:04:28 +0100

    fix orderId not sent in PayPal button and check for already processed payment

--- src/components/payment/PayPal.tsx
+++ src/components/payment/PayPal.tsx
@@ -1,14 +1,14 @@
 import {Typography} from "@mui/material";
 import {FUNDING, PayPalButtons, PayPalScriptProvider} from "@paypal/react-paypal-js";
-import React from "react";
+import React, {useRef} from "react";
 import axios from "axios";
 import {useAppDispatch, useAppSelector} from "../../store/hooks";
 import {IOrder, selectOrder, setOrderId} from "../../store/reducers/orderReducer";
 import {setPaymentStatus} from "../../store/reducers/paymentReducer";
 import {selectPersonalInformation, setUserId} from "../../store/reducers/personalInformationReducer";
 import {selectEventSelected} from "../../store/reducers/eventSelectionReducer";
-import {storeOrderAndUser} from "../../constants/util";
-import {OnApproveData} from "@paypal/paypal-js/types/components/buttons";
+import {storeOrderAndUser, validatePayment} from "../../constants/util";
+import {OnApproveData, OnClickActions} from "@paypal/paypal-js/types/components/buttons";
 
 export const PayPal = () => {
     const selectorOrder = useAppSelector(selectOrder);
@@ -16,12 +16,24 @@ export const PayPal = () => {
     const userInformation = useAppSelector(selectPersonalInformation);
     const dispatch = useAppDispatch();
 
+    const orderIdRef = useRef<string>(null);
+
+    const click = async (data, actions: OnClickActions) => {
+        const paymentAlreadyValid = await validatePayment(orderIdRef.current ? orderIdRef.current : selectorOrder.orderId);
+        if (paymentAlreadyValid) {
+            dispatch(setPaymentStatus("finished"));
+            return actions.reject();
+        }
+        return actions.resolve();
+    };
+
     const createOrder = async (): Promise<string> => {
         const {userId, orderId} = await storeOrderAndUser(selectorOrder, userInformation, selectedEvent, "paypal");
         dispatch(setOrderId(orderId));
         dispatch(setUserId(userId));
         const newOrder = Object.assign({}, selectorOrder) as IOrder;
         newOrder.orderId = orderId;
+        orderIdRef.current = orderId;
         const response = await axios.post("api/payment_intent/paypal", {order: newOrder});
         if (response.status === 201) {
             return null;
@@ -34,7 +46,11 @@ export const PayPal = () => {
     };
 
     const onApproved = async (data: OnApproveData) => {
-        await axios.post("api/webhook/paypal", {paypalId: data.orderID, orderId: selectorOrder.orderId});
+        const response = await axios.post("api/webhook/paypal", {paypalId: data.orderID, orderId: orderIdRef.current});
+        if (response.status !== 200) {
+            dispatch(setPaymentStatus("failure"));
+            return;
+        }
         dispatch(setPaymentStatus("finished"));
     };
 
@@ -53,6 +69,7 @@ export const PayPal = () => {
                     onError={onFailed}
                     onCancel={onFailed}
                     onApprove={onApproved}
+                    onClick={click}
                     fundingSource={FUNDING.PAYPAL}
                 />
             </PayPalScriptProvider>
