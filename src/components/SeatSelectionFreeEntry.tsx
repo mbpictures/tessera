857e3327883727156765a26a395418b1efe735a1
Marius Butz <mbpictures13@gmail.com>
2021-12-20 17:41:03 +0100

    implement first iteration of seat map seat selection

---
+++ src/components/SeatSelectionFreeEntry.tsx
@@ -0,0 +1,92 @@
+import {Button, IconButton, InputLabel, MenuItem, Paper, Select, Stack, TextField, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+import {Box} from "@mui/system";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import {FreeSeatOrder} from "../store/reducers/orderReducer";
+import {motion} from "framer-motion";
+import {Delete} from "@mui/icons-material";
+
+export const SeatSelectionFreeEntry = (
+    {
+        onChange,
+        categories,
+        index,
+        currentOrder,
+        onRemove
+    }: {
+        onChange?: (index: number, amount: number, categoryId) => unknown,
+        categories: Array<{ id: number, label: string, price: number }>,
+        index: number,
+        currentOrder: FreeSeatOrder,
+        onRemove?: (index: number) => unknown
+    }) => {
+    const [ticketAmount, setTicketAmount] = useState<number>(0);
+    const [category, setCategory] = useState<number>(categories[0].id);
+
+    useEffect(() => {
+        if (!currentOrder.orders || currentOrder.orders.length <= index) return;
+        if (currentOrder.orders[index].amount > 0)
+            setTicketAmount(currentOrder.orders[index].amount);
+        if (currentOrder.orders[index].categoryId != -1)
+            setCategory(currentOrder.orders[index].categoryId);
+    }, []);
+
+    useEffect(() => {
+        if (!onChange) return;
+        onChange(index, ticketAmount, category);
+    }, [ticketAmount, category]);
+
+    const handleChange = (event) => {
+        if (event.target.value === "") {
+            setTicketAmount(-1);
+            return;
+        }
+        const newValue = parseInt(event.target.value);
+        setTicketAmount(isNaN(newValue) ? 0 : newValue);
+    };
+
+    const onAdd = () => {
+        setTicketAmount(ticketAmount + 1);
+    };
+
+    const onSubtract = () => {
+        if (ticketAmount <= 0) return;
+        setTicketAmount(ticketAmount - 1);
+    };
+
+    const handleCategoryChange = (event) => {
+        setCategory(parseInt(event.target.value));
+    }
+
+    const price = ticketAmount * categories.find(value => value.id === category).price;
+
+    return (
+        <motion.div layout>
+            <Paper elevation={5} style={{display: "flex", padding: "20px", flexDirection: "column", margin: "20px 0"}}>
+                <Box style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    <TextField id="outlined-basic" label="Amount" variant="outlined" value={ticketAmount === -1 ? "" : ticketAmount} onChange={handleChange} />
+                    <Box width={20} />
+                    <Stack>
+                        <IconButton onClick={onAdd} color={"primary"}><AddCircleIcon fontSize={"large"} /></IconButton>
+                        <IconButton color="error" onClick={onSubtract}><RemoveCircleIcon fontSize={"large"} /></IconButton>
+                    </Stack>
+                </Box>
+                <InputLabel id="category-selection">Category</InputLabel>
+                <Select value={category} onChange={handleCategoryChange} id="category-selection">
+                    {categories.map(category => <MenuItem value={category.id} key={category.id}>{category.label} ({category.price}&euro;)</MenuItem>)}
+                </Select>
+                <motion.div layout style={{padding: "10px 0", alignSelf: "center"}}>
+                    {
+                        price > 0 && (
+                            <Typography variant={"body1"}>Price: <b>{price.toFixed(2)}&euro;</b></Typography>
+                        )
+                    }
+                </motion.div>
+                <Button startIcon={<Delete />} color={"error"} onClick={() => onRemove(index)} variant="outlined" style={{alignSelf: "center"}}>
+                    Remove Category
+                </Button>
+            </Paper>
+        </motion.div>
+    );
+}
---
+++ src/components/seatmap/SeatMapSeat.tsx
@@ -0,0 +1,54 @@
+import style from "./../../style/SeatMap.module.scss";
+import {motion} from "framer-motion";
+import {Tooltip, Typography} from "@mui/material";
+import React, {useState} from "react";
+
+export interface Seat {
+    type: "seat" | "space";
+    id?: number;
+    category?: number;
+    amount?: number;
+}
+
+export type OnSeatSelect = (seat: Seat, isSelected: boolean) => unknown;
+
+export const SeatMapSeat = ({seat, categories, onSeatSelect}: {seat: Seat, categories: Array<{id: number, label: string, price: number}>, onSeatSelect?: OnSeatSelect}) => {
+    const [isSelected, setIsSelected] = useState(false);
+
+    const handleSelect = () => {
+        if (onSeatSelect)
+            onSeatSelect(seat, !isSelected);
+        setIsSelected(prev => !prev);
+    };
+
+    const category = categories.find(category => category.id === seat.category);
+    if (!category) return null;
+    return (
+        <Tooltip
+            title={
+                <React.Fragment>
+                    <Typography variant={"body1"}>Category: {category.label}</Typography>
+                    <Typography variant={"body1"}>Price: {(category.price * seat.amount).toFixed(2)}€</Typography>
+                </React.Fragment>
+            }
+        >
+            <motion.div
+                className={style.seat}
+                style={{height: 40, width: (seat.amount ?? 1) * 40, backgroundColor: !isSelected ? "#59bb59" : "#5959bb"}}
+                whileHover={{
+                    opacity: 0.6,
+                    transition: { duration: 0.2, delay: 0 }
+                }}
+                onClick={handleSelect}
+            >
+                <span>{seat.id}</span>
+            </motion.div>
+        </Tooltip>
+    );
+};
+
+export const SeatMapSpace = ({seat}: {seat: Seat}) => {
+    return (
+        <div className={style.seat} style={{height: 40, width: (seat.amount ?? 1) * 40, cursor: "initial"}} />
+    );
+}
