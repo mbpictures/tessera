a60cb9ba0d97fdf7e8e2e3ad821bdbf593846fd5
Marius Butz <mbpictures13@gmail.com>
2021-12-20 21:45:06 +0100

    add basic add/remove seat functionality

--- src/components/seatmap/SeatMapSeat.tsx
+++ src/components/seatmap/SeatMapSeat.tsx
@@ -1,7 +1,9 @@
 import style from "./../../style/SeatMap.module.scss";
 import {motion} from "framer-motion";
 import {Tooltip, Typography} from "@mui/material";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {SeatOrder, selectOrder, setOrder} from "../../store/reducers/orderReducer";
 
 export interface Seat {
     type: "seat" | "space";
@@ -14,10 +16,54 @@ export type OnSeatSelect = (seat: Seat, isSelected: boolean) => unknown;
 
 export const SeatMapSeat = ({seat, categories, onSeatSelect}: {seat: Seat, categories: Array<{id: number, label: string, price: number}>, onSeatSelect?: OnSeatSelect}) => {
     const [isSelected, setIsSelected] = useState(false);
+    const order = useAppSelector(selectOrder) as SeatOrder;
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (order.seats) return;
+        const newOrder: SeatOrder = {
+            ticketAmount: -1,
+            totalPrice: -1,
+            seats: []
+        }
+        dispatch(setOrder(newOrder));
+    }, []);
+
+    const createNewOrder = () => {
+        return {ticketAmount: 0, seats: order.seats.map(a => a), totalPrice: 0};
+    };
+
+    const getPrice = (order: SeatOrder) => {
+        return order.seats
+            .map(seat => {
+                return {amount: seat.amount, price: categories.find(cat => cat.id === seat.category).price}
+            })
+            .reduce((a, seat) => a + seat.price, 0);
+    };
+
+    const addSeat = () => {
+        const newOrder: SeatOrder = createNewOrder();
+        newOrder.seats.push(seat);
+        newOrder.totalPrice = getPrice(newOrder);
+        newOrder.ticketAmount = newOrder.seats.length;
+        dispatch(setOrder(newOrder));
+    };
+
+    const removeSeat = () => {
+        const newOrder: SeatOrder = createNewOrder();
+        newOrder.seats.splice(newOrder.seats.findIndex(s => s.id === seat.id), 1);
+        newOrder.totalPrice = getPrice(newOrder);
+        newOrder.ticketAmount = newOrder.seats.length;
+        dispatch(setOrder(newOrder));
+    };
 
     const handleSelect = () => {
         if (onSeatSelect)
             onSeatSelect(seat, !isSelected);
+        if (isSelected)
+            removeSeat();
+        else
+            addSeat();
         setIsSelected(prev => !prev);
     };
 
--- src/components/seatmap/SeatSelectionMap.tsx
+++ src/components/seatmap/SeatSelectionMap.tsx
@@ -1,10 +1,14 @@
 import {SeatRow, SeatSelectionRow} from "./SeatSelectionRow";
 import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch";
 import {Card, Grid} from "@mui/material";
+import {useAppSelector} from "../../store/hooks";
+import {SeatOrder, selectOrder} from "../../store/reducers/orderReducer";
 
 export type SeatMap = Array<SeatRow>;
 
 export const SeatSelectionMap = ({seatSelectionDefinition, categories}: {seatSelectionDefinition: SeatMap, categories: Array<{id: number, label: string, price: number}>}) => {
+    const order = useAppSelector(selectOrder) as SeatOrder;
+
     return (
         <Grid container>
             <Grid item md={12} lg={8} style={{width: "100%"}}>
@@ -20,7 +24,7 @@ export const SeatSelectionMap = ({seatSelectionDefinition, categories}: {seatSel
             </Grid>
             <Grid item md={12} lg={4} display="flex" alignItems="center">
                 <Card style={{flex: "1 1 auto", padding: "10px"}}>
-                    Hallo
+                    {order.totalPrice} €
                 </Card>
             </Grid>
         </Grid>
