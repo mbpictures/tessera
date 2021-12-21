a4b24461ec962f7cdf18c232dfcfbb7ae652d7c7
Marius Butz <mbpictures13@gmail.com>
2021-12-21 20:12:26 +0100

    refactor: move handling of adding and removing seats to seat selection map

--- src/components/seatmap/SeatSelectionRow.tsx
+++ src/components/seatmap/SeatSelectionRow.tsx
@@ -1,13 +1,13 @@
-import {Seat} from "./SeatMapSeat";
+import {OnSeatSelect, Seat} from "./SeatMapSeat";
 import {SeatMapSeatTypeFactory} from "./SeatMapSeatTypeFactory";
 
 export type SeatRow = Array<Seat>;
 
-export const SeatSelectionRow = ({row, categories}: {row: SeatRow, categories: Array<{id: number, label: string, price: number}>}) => {
+export const SeatSelectionRow = ({row, categories, onSelectSeat}: {row: SeatRow, categories: Array<{id: number, label: string, price: number}>, onSelectSeat?: OnSeatSelect}) => {
     return (
         <div style={{display: "flex"}}>
             {
-                row.map((seat, index) => <SeatMapSeatTypeFactory key={`seat${index}`} data={seat} categories={categories} />)
+                row.map((seat, index) => <SeatMapSeatTypeFactory key={`seat${index}`} data={seat} categories={categories} onSelectSeat={onSelectSeat} />)
             }
         </div>
     )
