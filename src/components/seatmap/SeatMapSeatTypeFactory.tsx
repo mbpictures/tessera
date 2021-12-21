de1d49c4a9688065bca4022d4e0919185c10e5f4
Marius Butz <mbpictures13@gmail.com>
2021-12-21 20:12:26 +0100

    refactor: move handling of adding and removing seats to seat selection map

--- src/components/seatmap/SeatMapSeatTypeFactory.tsx
+++ src/components/seatmap/SeatMapSeatTypeFactory.tsx
@@ -1,12 +1,12 @@
-import {Seat, SeatMapSeat, SeatMapSpace} from "./SeatMapSeat";
+import {OnSeatSelect, Seat, SeatMapSeat, SeatMapSpace} from "./SeatMapSeat";
 
 interface SeatType extends Seat {
     type: "seat" | "space";
 }
 
-export const SeatMapSeatTypeFactory = ({data, categories}: {data: SeatType, categories}) => {
+export const SeatMapSeatTypeFactory = ({data, categories, onSelectSeat}: {data: SeatType, categories, onSelectSeat?: OnSeatSelect}) => {
     if (data.type === "seat") {
-        return <SeatMapSeat seat={data} categories={categories} />
+        return <SeatMapSeat seat={data} categories={categories} onSeatSelect={onSelectSeat} />
     }
     if (data.type === "space") {
         return <SeatMapSpace seat={data} />
--- src/components/seatmap/SeatSelectionMap.tsx
+++ src/components/seatmap/SeatSelectionMap.tsx
@@ -1,13 +1,62 @@
 import {SeatRow, SeatSelectionRow} from "./SeatSelectionRow";
 import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch";
 import {Card, Grid} from "@mui/material";
-import {useAppSelector} from "../../store/hooks";
-import {SeatOrder, selectOrder} from "../../store/reducers/orderReducer";
+import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {SeatOrder, selectOrder, setOrder} from "../../store/reducers/orderReducer";
+import {useEffect} from "react";
+import {Seat} from "./SeatMapSeat";
 
 export type SeatMap = Array<SeatRow>;
 
 export const SeatSelectionMap = ({seatSelectionDefinition, categories}: {seatSelectionDefinition: SeatMap, categories: Array<{id: number, label: string, price: number}>}) => {
     const order = useAppSelector(selectOrder) as SeatOrder;
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (order.seats) return;
+        const newOrder: SeatOrder = {
+            ticketAmount: -1,
+            totalPrice: -1,
+            seats: []
+        }
+        dispatch(setOrder(newOrder));
+    }, []);
+
+    const createNewOrder = () => {
+        return {ticketAmount: 0, seats: order.seats.map(a => a), totalPrice: 0};
+    };
+
+    const getPrice = (order: SeatOrder) => {
+        return order.seats
+            .map(seat => {
+                return {amount: seat.amount, price: categories.find(cat => cat.id === seat.category).price}
+            })
+            .reduce((a, seat) => a + seat.price, 0);
+    };
+
+    const addSeat = (seat: Seat) => {
+        const newOrder: SeatOrder = createNewOrder();
+        newOrder.seats.push(seat);
+        newOrder.totalPrice = getPrice(newOrder);
+        newOrder.ticketAmount = newOrder.seats.length;
+        dispatch(setOrder(newOrder));
+    };
+
+    const removeSeat = (seat: Seat) => {
+        const newOrder: SeatOrder = createNewOrder();
+        newOrder.seats.splice(newOrder.seats.findIndex(s => s.id === seat.id), 1);
+        newOrder.totalPrice = getPrice(newOrder);
+        newOrder.ticketAmount = newOrder.seats.length;
+        dispatch(setOrder(newOrder));
+    };
+
+    const handleSelectSeat = (seat: Seat, isSelected: boolean) => {
+        if (isSelected) {
+            addSeat(seat);
+            return;
+        }
+        removeSeat(seat);
+    };
 
     return (
         <Grid container>
@@ -16,7 +65,7 @@ export const SeatSelectionMap = ({seatSelectionDefinition, categories}: {seatSel
                     <TransformComponent wrapperStyle={{width: "100%"}}>
                         <div style={{display: "flex", flexDirection: "column"}}>
                             {
-                                seatSelectionDefinition.map((row, index) => <SeatSelectionRow key={`row${index}`} row={row} categories={categories} />)
+                                seatSelectionDefinition.map((row, index) => <SeatSelectionRow key={`row${index}`} row={row} categories={categories} onSelectSeat={handleSelectSeat} />)
                             }
                         </div>
                     </TransformComponent>
