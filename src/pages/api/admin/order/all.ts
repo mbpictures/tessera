ccef80cf27b70f528fd54d34ef0fe616a9816a4b
Marius Butz <mbpictures13@gmail.com>
2021-12-31 16:09:33 +0100

    add api route to delete all orders

---
+++ src/pages/api/admin/order/all.ts
@@ -0,0 +1,44 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {serverAuthenticate} from "../../../../constants/serverUtil";
+import prisma from "../../../../lib/prisma";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const user = await serverAuthenticate(req);
+    if (!user) {
+        res.status(401).end("Unauthenticated");
+        return;
+    }
+
+    if (req.method !== "DELETE") {
+        res.status(400).end("Method unsupported");
+        return;
+    }
+
+    try {
+        const orders = await prisma.order.findMany({include: {tickets: true}});
+        const promises = orders.map(async (order) => {
+            // delete all tickets for order
+            await Promise.all(order.tickets.map(async (ticket) => {
+                await prisma.ticket.delete({
+                    where: {
+                        id: ticket.id
+                    }
+                })
+            }));
+            // delete order
+            await prisma.order.delete({
+                where: {
+                    id: order.id
+                }
+            });
+        });
+        await Promise.all(promises);
+        res.status(200).end("Deleted");
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+    }
+}
