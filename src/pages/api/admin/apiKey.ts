cee5c5508bd684cc149902580d40c7cb945674f0
Marius Butz <mbpictures13@gmail.com>
2021-12-31 01:06:04 +0100

    add api endpoint to add and remove admin api keys

---
+++ src/pages/api/admin/apiKey.ts
@@ -0,0 +1,53 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {hashPassword, serverAuthenticate} from "../../../constants/serverUtil";
+import prisma from "../../../lib/prisma";
+import * as crypto from "crypto";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+){
+    const user = await serverAuthenticate(req);
+    if (!user) {
+        res.status(401).end("Not Authenticated");
+        return;
+    }
+
+    if (req.method === "POST") {
+        try {
+            const {name} = req.body;
+            const token = crypto.randomUUID();
+
+            await prisma.adminApiKeys.create({
+                data: {
+                    key: await hashPassword(token),
+                    name: name,
+                    user: {
+                        connect: {
+                            email: user.email
+                        }
+                    }
+                },
+            });
+            res.status(200).json({token: token});
+        } catch (e) {
+            res.status(500).end("Server error");
+        }
+    }
+
+    if (req.method === "DELETE") {
+        try {
+            const {id} = req.body;
+            await prisma.adminApiKeys.delete({
+                where: {
+                    id: id
+                }
+            });
+            res.status(200).end("Deleted");
+        } catch (e) {
+            res.status(500).end("Server error");
+        }
+    }
+
+    res.status(400).end("Method unsupported");
+}
