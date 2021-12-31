b5184bb9806d3d435906ac2c3574ed1a674ee819
Marius Butz <mbpictures13@gmail.com>
2021-12-31 01:04:42 +0100

    add session parameter to result function

--- src/constants/serverUtil.ts
+++ src/constants/serverUtil.ts
@@ -34,7 +34,7 @@ export const getAdminServerSideProps = async (context, resultFunction?) => {
         }
     }
 
-    const result = resultFunction ? await resultFunction() : {};
+    const result = resultFunction ? await resultFunction(session) : {};
     if (!result.props)
         result.props = {};
 
