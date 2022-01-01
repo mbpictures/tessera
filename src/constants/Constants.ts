5f9a42d2fa0f6c81385c6669ecf98bd75a0cee93
Marius Butz <mbpictures13@gmail.com>
2022-01-02 00:36:42 +0100

    move default seat map colors to constants

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,3 +1,9 @@
 export const EVENT_SELECTION_KEY = "event_selection";
 export const STEPS = ["Select Event", "Choose seat", "Personal Information", "Payment", "Checkout Order"];
 export const STEP_URLS = ["/", "/seatselection", "/information", "/payment", "/checkout"];
+
+export const SEAT_COLORS = {
+    normal: "#59bb59",
+    active: "#5959bb",
+    occupied: "#FF2222"
+}
