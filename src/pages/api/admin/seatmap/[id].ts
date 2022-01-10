9d8437cc443dfbac5771ee04868e93dd172d9e6b
Marius Butz <mbpictures13@gmail.com>
2022-01-10 20:38:17 +0100

    add api endpoints to manipulate seatmaps

---
+++ src/pages/api/admin/seatmap/[id].ts
@@ -0,0 +1,65 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {serverAuthenticate} from "../../../../constants/serverUtil";
+import prisma from "../../../../lib/prisma";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+){
+    const user = await serverAuthenticate(req);
+    if (!user) {
+        res.status(401).end("Unauthenticated");
+        return;
+    }
+
+    const {id} = req.query;
+    const seatMap = await prisma.seatMap.findUnique({
+        where: {
+            id: parseInt(id as string)
+        },
+        include: {
+            events: true
+        }
+    });
+
+    if (!seatMap) {
+        res.status(404).end("Category not found");
+        return;
+    }
+
+
+    if (req.method === "GET") {
+        res.status(200).json(seatMap);
+        return;
+    }
+
+    if (req.method === "DELETE") {
+        if (seatMap.events.length > 0) {
+            res.status(400).end("Category is in use by events!");
+            return;
+        }
+        await prisma.seatMap.delete({
+            where: {
+                id: parseInt(id as string)
+            }
+        });
+        res.status(200).end("Deleted");
+        return;
+    }
+
+    if (req.method === "PUT") {
+        const {definition} = req.body;
+        await prisma.category.update({
+            where: {
+                id: parseInt(id as string)
+            },
+            data: {
+                ...(definition && {definition: JSON.stringify(definition)}),
+            }
+        });
+        res.status(200).end("Updated");
+        return;
+    }
+
+    res.status(400).end("Method unsupported");
+}
