9446d8b96083c09984b7bb26cc4c00940862aa3a
Marius Butz <mbpictures13@gmail.com>
2022-01-10 20:38:17 +0100

    add api endpoints to manipulate seatmaps

---
+++ src/pages/api/admin/seatmap/index.ts
@@ -0,0 +1,30 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {serverAuthenticate} from "../../../../constants/serverUtil";
+import prisma from "../../../../lib/prisma";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const user = await serverAuthenticate(req);
+    if (!user) {
+        res.status(401).end("Unauthenticated");
+        return;
+    }
+
+    if (req.method === "GET") {
+        const seatMaps = await prisma.seatMap.findMany();
+        res.status(200).json(seatMaps);
+        return;
+    }
+
+    if (req.method === "POST") {
+        const {definition} = req.body;
+        const seatMap = await prisma.seatMap.create({
+            data: {
+                ...(definition && {definition: JSON.stringify(definition)})
+            }
+        });
+        res.status(200).end(seatMap.id.toFixed(0));
+    }
+}
