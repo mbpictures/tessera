f0cdc808eba192b8d34a8383d62810193e0ab011
Marius <mbpictures13@gmail.com>
2022-01-12 14:19:53 +0100

    fix register button in login page missing due styling

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -13,14 +13,13 @@ const RootStyle = styled('div')(({ theme }) => ({
     }
 }));
 
-const ContentStyle = styled('div')(({ theme }) => ({
+const ContentStyle = styled('div')(() => ({
     maxWidth: 480,
     margin: 'auto',
     display: 'flex',
     minHeight: '100vh',
     flexDirection: 'column',
-    justifyContent: 'center',
-    padding: theme.spacing(12, 0)
+    justifyContent: 'center'
 }));
 
 export default function Login({noUser}) {
