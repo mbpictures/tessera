21e35c3c4968706a0684546fd5b696ebd6674963
Marius Butz <mbpictures13@gmail.com>
2022-01-12 21:40:16 +0100

    add popup to add events in admin view

---
+++ src/components/admin/dialogs/AddEventDialog.tsx
@@ -0,0 +1,47 @@
+import {useState} from "react";
+import {useSnackbar} from "notistack";
+import {Button, Dialog, DialogContent, DialogTitle, MenuItem, Select, Stack, TextField} from "@mui/material";
+import axios from "axios";
+
+export const AddEventDialog = ({open, onClose, onChange}) => {
+    const [name, setName] = useState("");
+    const [seatType, setSeatType] = useState("free");
+    const { enqueueSnackbar } = useSnackbar();
+
+    const handleClose = () => {
+        setName("");
+        onClose();
+    };
+
+    const handleAdd = async () => {
+        try {
+            await axios.post("/api/admin/events", {title: name, seatType: seatType});
+            onChange();
+            handleClose();
+        } catch (e) {
+            enqueueSnackbar("Error: " + (e.response.data ?? e.message), {variant: "error"});
+        }
+    };
+
+    return (
+        <>
+            <Dialog open={open} onClose={handleClose} fullWidth>
+                <DialogTitle>Create New Event</DialogTitle>
+                <DialogContent>
+                    <Stack pt={1} pb={1} spacing={1}>
+                        <TextField
+                            value={name}
+                            onChange={event => setName(event.target.value)}
+                            label={"Name"}
+                        />
+                        <Select value={seatType} onChange={(event) => setSeatType(event.target.value)}>
+                            <MenuItem value={"free"}>Free</MenuItem>
+                            <MenuItem value={"seatmap"}>Seat map</MenuItem>
+                        </Select>
+                        <Button onClick={handleAdd}>Add Event</Button>
+                    </Stack>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
+};
--- src/pages/admin/events/index.tsx
+++ src/pages/admin/events/index.tsx
@@ -1,21 +1,36 @@
 import {useSession} from "next-auth/react";
 import {AdminLayout} from "../../../components/admin/layout";
-import {Box, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography} from "@mui/material";
+import {Box, Button, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography} from "@mui/material";
 import {getAdminServerSideProps} from "../../../constants/serverUtil";
 import InfoIcon from "@mui/icons-material/Info";
 import prisma from "../../../lib/prisma";
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import {AddEventDialog} from "../../../components/admin/dialogs/AddEventDialog";
+import {useState} from "react";
+import {useRouter} from "next/router";
 
 export default function events({events}) {
     const {data: session} = useSession();
+    const [addEventOpen, setAddEventOpen] = useState(false);
+    const router = useRouter();
 
     if (!session) return null;
 
+    const refreshProps = async () => {
+        await router.replace(router.asPath);
+    };
+
     return (
         <AdminLayout>
+            <AddEventDialog open={addEventOpen} onClose={() => setAddEventOpen(false)} onChange={refreshProps} />
             <Box sx={{ pb: 5 }}>
                 <Typography variant="h4">Events</Typography>
+            </Box>
+            <Box display={"flex"}>
+                <Box flexGrow={1} />
+                <Button onClick={() => setAddEventOpen(true)}><AddIcon /> Add Seat Map</Button>
             </Box>
             <Box>
                 {
