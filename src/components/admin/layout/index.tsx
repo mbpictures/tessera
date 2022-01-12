26adb6982cb9697f00f61b862cf17ab428bc2310
Marius <mbpictures13@gmail.com>
2022-01-12 15:39:07 +0100

    add padding in admin pages for small devices

--- src/components/admin/layout/index.tsx
+++ src/components/admin/layout/index.tsx
@@ -22,9 +22,9 @@ const MainStyle = styled('div')(({ theme }) => ({
     paddingBottom: theme.spacing(10),
     [theme.breakpoints.up('lg')]: {
         paddingTop: APP_BAR_DESKTOP + 24,
-        paddingLeft: theme.spacing(2),
-        paddingRight: theme.spacing(2)
-    }
+    },
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
 }));
 
 const additionalPages = [
