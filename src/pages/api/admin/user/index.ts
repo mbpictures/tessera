686772ca6fce950ad433dcb2858bed81ede5f8df
Marius Butz <mbpictures13@gmail.com>
2022-01-16 13:37:33 +0100

    add rights to first admin user registered

--- src/pages/api/admin/user/index.ts
+++ src/pages/api/admin/user/index.ts
@@ -21,11 +21,21 @@ export default async function handler(
 
     if (req.method === "POST") {
         const {username, email, password} = req.body;
+        let additionalData = {};
+        if (canRegister) {
+            // first user needs to have all rights (otherwise he can't do anything)
+            additionalData = {
+                readRights: Object.values(PermissionSection).filter(permission => permission !== PermissionSection.None),
+                writeRights: Object.values(PermissionSection).filter(permission => permission !== PermissionSection.None),
+            }
+        }
+
         const user = await prisma.adminUser.create({
             data: {
                 userName: username,
                 password: await hashPassword(password),
-                email: email
+                email: email,
+                ...additionalData
             }
         });
         res.status(200).end(user.id.toFixed(0));
