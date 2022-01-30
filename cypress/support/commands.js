3037b93ccba6bdbb29ac8fb00def93fe23f61242
Marius Butz <mbpictures13@gmail.com>
2022-01-31 00:07:44 +0100

    test: add commands to register user, create token, create events

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -37,3 +37,116 @@ Cypress.Commands.add("logout", () => {
     cy.get("#logout-button").click();
     cy.url().should("eq", Cypress.config().baseUrl + "/admin/login");
 });
+
+Cypress.Commands.add("registerInitialAdminUser", () => {
+    cy.fixture("admin/user").then((userFixture) => {
+        cy.request("POST", "api/admin/user", userFixture).then(({ status }) => {
+            expect(status).to.equal(200);
+        });
+    });
+});
+
+Cypress.Commands.add("createToken", () => {
+    cy.fixture("admin/user").then((userFixture) => {
+        cy.login(userFixture.email, userFixture.password);
+        cy.url().should("eq", Cypress.config().baseUrl + "/admin")
+        cy.visit("/admin/user/settings");
+        cy.get(".MuiAccordion-root").last().click();
+        cy.get("#add-api-key-button").click();
+        cy.get("#api-key-name").type("test");
+        cy.get("#api-key-generate").click();
+        cy.get("#api-key-token").then((text) => {
+            cy.task("setAdminToken", text.text());
+        })
+    });
+});
+
+Cypress.Commands.add("createEvents", () => {
+    cy.fixture("admin/user").then((userFixture) => {
+        cy.fixture("admin/events").then((eventsFixture) => {
+            cy.task("getAdminToken").then( async (token) => {
+                for(let i = 0; i < eventsFixture.categories.length; i++) {
+                    cy.request(
+                        {
+                            url: "/api/admin/category",
+                            method: "POST",
+                            headers: {
+                                "Authorization": `Bearer ${userFixture.username}:${token}`
+                            },
+                            body: eventsFixture.categories[i]
+                        }
+                    )
+                }
+                eventsFixture.events.forEach((event) => {
+                    cy.request(
+                        {
+                            url: "/api/admin/category",
+                            method: "GET",
+                            headers: {
+                                "Authorization": `Bearer ${userFixture.username}:${token}`
+                            }
+                        }
+                    ).then((categoryResponse) => {
+                        const categoryIds = categoryResponse.body.map(category => category.id);
+                        const eventData = {
+                            title: event.title,
+                            seatType: event.seatType
+                        };
+                        let updateData = {};
+                        if ("seatMap" in event) {
+                            const seatMapDefinition = event.seatMap.map((row) => {
+                                return row.map((seat) => {
+                                    return {
+                                        ...seat,
+                                        category: categoryIds[seat.category]
+                                    }
+                                });
+                            })
+                            cy.request(
+                                {
+                                    url: "/api/admin/seatmap",
+                                    method: "POST",
+                                    headers: {
+                                        "Authorization": `Bearer ${userFixture.username}:${token}`
+                                    },
+                                    body: {
+                                        definition: seatMapDefinition
+                                    }
+                                }
+                            ).then(({ body }) => {
+                                updateData.seatMapId = parseInt(body);
+                            });
+                        }
+                        if ("categories" in event) {
+                            updateData.categories = event.categories.map((_, index) => categoryIds[index]);
+                        }
+
+                        cy.log(updateData);
+
+                        cy.request(
+                            {
+                                url: "/api/admin/events",
+                                method: "POST",
+                                headers: {
+                                    "Authorization": `Bearer ${userFixture.username}:${token}`
+                                },
+                                body: eventData
+                            }
+                        ).then(({ body }) => {
+                            cy.request(
+                                {
+                                    url: "/api/admin/events/" + body,
+                                    method: "PUT",
+                                    headers: {
+                                        "Authorization": `Bearer ${userFixture.username}:${token}`
+                                    },
+                                    body: updateData
+                                }
+                            );
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
