a5fff704583b6454cfb3758973d7570925fac9da
Marius Butz <mbpictures13@gmail.com>
2022-01-31 22:40:58 +0100

    test: add test for free seat selection

--- cypress/integration/shop/index.js
+++ cypress/integration/shop/index.js
@@ -1,3 +1,5 @@
+import { formatPrice } from "../../../src/constants/util";
+
 describe("Buy tickets", () => {
     before(() => {
         cy.task("db:teardown", null, {timeout: 30000});
@@ -7,7 +9,7 @@ describe("Buy tickets", () => {
     });
 
     it("Events exist", () => {
-        cy.visit("/");
+        cy.visit("/", {timeout: 8000});
 
         cy.fixture("admin/events").then((events) => {
             cy.get("input[name=event_selection]").should("have.length", 2);
@@ -20,24 +22,61 @@ describe("Buy tickets", () => {
     });
 
     it("Selecting Event", () => {
-        cy.visit("/");
+        cy.visit("/", {timeout: 8000});
 
         cy.get("input[name=event_selection]").first().parent().click();
+        cy.get("input[name=event_selection]").first().should("be.checked");
         cy.get("#stepper-next-button").click();
-        cy.url().should("include", "seatselection");
+        cy.url({timeout: 8000}).should("include", "seatselection");
         cy.location().should((url) => {
             const queries = new URL(url.toString());
             expect(parseInt(queries.searchParams.get("event"))).to.equal(1);
         });
 
-        cy.visit("/");
+        cy.visit("/", {timeout: 8000});
 
         cy.get("input[name=event_selection]").last().parent().click();
+        cy.get("input[name=event_selection]").last().should("be.checked");
         cy.get("#stepper-next-button").click();
-        cy.url().should("include", "seatselection");
+        cy.url({timeout: 8000}).should("include", "seatselection");
         cy.location().should((url) => {
             const queries = new URL(url.toString());
             expect(parseInt(queries.searchParams.get("event"))).to.equal(2);
+        });
+    });
+
+    it("Select Free Seats", () => {
+        cy.visit("/seatselection?event=1");
+
+        cy.get(".seat-selection-free-add").first().click();
+        cy.get(".seat-selection-free-ticket-amount input").should("have.value", "1");
+        cy.get(".seat-selection-free-add").first().click();
+        cy.get(".seat-selection-free-ticket-amount input").should("have.value", "2");
+        cy.get(".seat-selection-free-remove").first().click();
+        cy.get(".seat-selection-free-ticket-amount input").should("have.value", "1");
+
+        cy.fixture("admin/events").then((eventFixture) => {
+            cy.get(".category-selection").first().click();
+            cy.get("#category-selection-entry0-1").click();
+
+            const firstCategory = eventFixture.categories[0];
+            const secondCategory = eventFixture.categories[1];
+            cy.get("#seat-selection-free-total-price").should(
+                "have.text",
+                formatPrice(firstCategory.price, firstCategory.currency)
+            );
+
+            cy.get(".category-selection").first().click();
+            cy.get("#category-selection-entry0-2").click();
+
+            cy.get("#seat-selection-free-total-price").should(
+                "have.text",
+                formatPrice(secondCategory.price, secondCategory.currency)
+            );
+
+            cy.get("#stepper-next-button").should("not.be.disabled");
+            cy.get(".seat-selection-free-remove").first().click();
+            cy.get("#stepper-next-button").should("be.disabled");
         });
     });
 });
