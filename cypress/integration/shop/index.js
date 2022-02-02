c016338f7157352f0b93ba280ee90150ee4e8bca
Marius Butz <mbpictures13@gmail.com>
2022-02-02 21:43:23 +0100

    test: add tests for seat map selection

--- cypress/integration/shop/index.js
+++ cypress/integration/shop/index.js
@@ -1,5 +1,25 @@
 import { formatPrice } from "../../../src/constants/util";
 
+function RGBToHex(rgb) {
+    // Choose correct separator
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    // Turn "rgb(r,g,b)" into [r,g,b]
+    rgb = rgb.substr(4).split(")")[0].split(sep);
+
+    let r = (+rgb[0]).toString(16),
+        g = (+rgb[1]).toString(16),
+        b = (+rgb[2]).toString(16);
+
+    if (r.length === 1)
+        r = "0" + r;
+    if (g.length === 1)
+        g = "0" + g;
+    if (b.length === 1)
+        b = "0" + b;
+
+    return ("#" + r + g + b).toLowerCase();
+}
+
 describe("Buy tickets", () => {
     before(() => {
         cy.task("db:teardown", null, {timeout: 30000});
@@ -78,5 +98,36 @@ describe("Buy tickets", () => {
             cy.get(".seat-selection-free-remove").first().click();
             cy.get("#stepper-next-button").should("be.disabled");
         });
+    });
+
+    it("Select Seat Map", () => {
+        cy.fixture("admin/events").then((eventsFixture) => {
+            const seats = eventsFixture.events[1].seatMap.flat(2);
+            cy.visit("/seatselection?event=2");
+
+            const selectedIndexes = [0, 1, 2, 3];
+            selectedIndexes.forEach((num) => {
+                const category = eventsFixture.categories[seats[num].category];
+                cy.get(".seat-selection-seatmap-seat").eq(num).then(elem => {
+                    expect(RGBToHex(elem[0].style.backgroundColor)).to.equal(category.color.toLowerCase());
+                });
+                cy.get(".seat-selection-seatmap-seat").eq(num).click();
+                cy.get(".seat-selection-seatmap-seat").eq(num).then(elem => {
+                    expect(RGBToHex(elem[0].style.backgroundColor)).to.equal(category.activeColor.toLowerCase());
+                });
+            });
+
+            const price = seats
+                .filter((_, index) => selectedIndexes.includes(index))
+                .reduce((a, b) => a + eventsFixture.categories[b.category].price, 0);
+            cy.get("#payment-overview-total-price").should(
+                "have.text",
+                formatPrice(price, eventsFixture.categories[0].currency)
+            );
+
+            const categoryLabel = eventsFixture.categories[seats[0].category].label;
+            cy.get("#payment-overview-category-amount-" + categoryLabel)
+                .should("have.text", `${selectedIndexes.length}x: ${categoryLabel}`)
+        })
     });
 });
