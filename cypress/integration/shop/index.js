aeecfdc55fded0446de50b1f23f1410f9cb5ae31
Marius Butz <mbpictures13@gmail.com>
2022-02-07 01:34:10 +0100

    test: add tests for personal information fill out and stripe credit card payment method

--- cypress/integration/shop/index.js
+++ cypress/integration/shop/index.js
@@ -1,4 +1,5 @@
 import { formatPrice } from "../../../src/constants/util";
+import { faker } from '@faker-js/faker';
 
 function RGBToHex(rgb) {
     // Choose correct separator
@@ -42,23 +43,23 @@ describe("Buy tickets", () => {
     });
 
     it("Selecting Event", () => {
-        cy.visit("/", {timeout: 8000});
+        cy.visit("/");
 
         cy.get("input[name=event_selection]").first().parent().click();
         cy.get("input[name=event_selection]").first().should("be.checked");
         cy.get("#stepper-next-button").click();
-        cy.url({timeout: 8000}).should("include", "seatselection");
+        cy.url().should("include", "seatselection");
         cy.location().should((url) => {
             const queries = new URL(url.toString());
             expect(parseInt(queries.searchParams.get("event"))).to.equal(1);
         });
 
-        cy.visit("/", {timeout: 8000});
+        cy.visit("/");
 
         cy.get("input[name=event_selection]").last().parent().click();
         cy.get("input[name=event_selection]").last().should("be.checked");
         cy.get("#stepper-next-button").click();
-        cy.url({timeout: 8000}).should("include", "seatselection");
+        cy.url().should("include", "seatselection");
         cy.location().should((url) => {
             const queries = new URL(url.toString());
             expect(parseInt(queries.searchParams.get("event"))).to.equal(2);
@@ -129,5 +130,88 @@ describe("Buy tickets", () => {
             cy.get("#payment-overview-category-amount-" + categoryLabel)
                 .should("have.text", `${selectedIndexes.length}x: ${categoryLabel}`)
         })
+    });
+
+    it("Enter Information", () => {
+        cy.visit("/seatselection?event=1");
+        cy.get(".seat-selection-free-add").first().click();
+        cy.get("#stepper-next-button").click();
+        cy.url().should("include", "information");
+
+        const testInputField = (element, notValidInput, validInput, errorText) => {
+            cy.get(element).type(notValidInput);
+            cy.contains(errorText).should("exist");
+            cy.get(element).clear();
+            cy.get(element).type(validInput);
+            cy.contains(errorText).should("not.exist");
+        }
+
+        testInputField("input[name=address-email]", "not_valid", faker.internet.email(), "Please enter valid email address");
+        testInputField("input[name=address-firstname]", "a", faker.name.firstName(), "Please enter your firstname here!");
+        testInputField("input[name=address-lastname]", "a", faker.name.lastName(), "Please enter your lastname here!");
+        testInputField("input[name=address-address]", "aaaaaa", faker.address.streetAddress(), "Please enter street and house number!");
+        testInputField("input[name=address-zip]", "5445", faker.address.zipCode("#####"), "Please enter a valid ZIP Code");
+        testInputField("input[name=address-city]", "aa", faker.address.city(), "Please enter your city!");
+
+        cy.get("input[name=address-country-text").type("Germany");
+        cy.get(".MuiAutocomplete-popper").children().first().click();
+
+        cy.get("input[name=address-region-text").type("Rheinland");
+        cy.get(".MuiAutocomplete-popper").children().first().click();
+
+        cy.get("#checkbox-download").click();
+        cy.get("#stepper-next-button").should("be.enabled");
+        cy.get("#checkbox-download").click();
+        cy.get("#stepper-next-button").should("not.be.enabled");
+        cy.get("#checkbox-download").click();
+        cy.get("#stepper-next-button").should("be.enabled");
+    });
+
+    it("Select Payment", () => {
+        cy.visit("/seatselection?event=1");
+        cy.get(".seat-selection-free-add").first().click();
+        cy.get("#stepper-next-button").click();
+        cy.url().should("include", "information");
+
+        cy.get("input[name=address-email]").type(faker.internet.email());
+        cy.get("input[name=address-firstname]").type(faker.name.firstName());
+        cy.get("input[name=address-lastname]").type(faker.name.lastName());
+        cy.get("input[name=address-address]").type(faker.address.streetAddress());
+        cy.get("input[name=address-zip]").type(faker.address.zipCode("#####"));
+        cy.get("input[name=address-city]").type(faker.address.city());
+
+        cy.get("input[name=address-country-text").type("Germany");
+        cy.get(".MuiAutocomplete-popper").children().first().click();
+
+        cy.get("input[name=address-region-text").type("Rheinland");
+        cy.get(".MuiAutocomplete-popper").children().first().click();
+
+        cy.get("#checkbox-download").click();
+        cy.get("#stepper-next-button").click();
+
+        cy.url().should("include", "payment");
+
+        cy.get("#pay-button").should("be.disabled");
+        cy.get("#checkbox-creditcard").click();
+        cy.get("#stripe-card-name").type(faker.name.findName());
+        cy.get("#stripe-card-number iframe").then(iframe => {
+            cy.wrap(iframe.contents().find("input[name=cardnumber]"))
+                .type(`4242424242424242`)
+        });
+        //cy.get("input[name=cardnumber]").type("4242424242424242");
+        const expirationData = new Date(Date.now() + 1000*60*60*24*30*6);
+        cy.get("#stripe-card-expire iframe").then(iframe => {
+            cy.wrap(iframe.contents().find("input[name=exp-date]"))
+                .type(`${expirationData.getMonth()}-${expirationData.getFullYear().toString().substr(-2)}`)
+        });
+        //cy.get("input[name=exp-date]")
+        //    .type(`${expirationData.getMonth()}-${expirationData.getFullYear().toString().substr(-2)}`);
+        cy.get("#stripe-card-cvc iframe").then(iframe => {
+            cy.wrap(iframe.contents().find("input[name=cvc]"))
+                .type(faker.finance.creditCardCVV())
+        });
+        //cy.get("input[cvc]").type(faker.finance.creditCardCVV());
+
+        cy.get("#pay-button").should("be.enabled");
     });
 });
