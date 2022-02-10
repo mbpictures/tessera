514bb21b555f402b102bf193635e8b3c8493b132
Marius Butz <mbpictures13@gmail.com>
2022-02-10 16:53:09 +0100

    test: add price format tests

--- cypress/integration/shop/util.test.js
+++ cypress/integration/shop/util.test.js
@@ -1,4 +1,4 @@
-import { arrayEquals, calculateTotalPrice, totalTicketAmount } from "../../../src/constants/util";
+import { arrayEquals, calculateTotalPrice, formatPrice, totalTicketAmount } from "../../../src/constants/util";
 
 describe("Utils", () => {
     it("Calculate Total Price", () => {
@@ -53,7 +53,23 @@ describe("Utils", () => {
         expect(calculateTotalPrice(freeSeatOrder, categories)).to.equal(
             price
         );
-        expect(totalTicketAmount({})).to.equal(-1);
+        const freeSeatOrder2 = {
+            orders: [
+                {
+                    categoryId: 1,
+                    amount: 3
+                },
+                {
+                    categoryId: 3,
+                    amount: 3,
+                    price: 19.99
+                }
+            ]
+        };
+        expect(calculateTotalPrice(freeSeatOrder2, categories)).to.equal(
+            categories[0].price * 3 + 19.99
+        )
+        expect(calculateTotalPrice({})).to.equal(-1);
     })
 
     it("Total Ticket Amount", () => {
@@ -102,5 +118,31 @@ describe("Utils", () => {
         expect(arrayEquals([1, 3], [2, 1])).to.equal(false);
         expect(arrayEquals([1, 2, 1], [1, 2])).to.equal(false);
         expect(arrayEquals(2, [2])).to.equal(false);
+    })
+
+    it("Format Price", () => {
+        Object.defineProperty(navigator, "language", {
+            value: "en-US",
+            configurable: true
+        });
+
+        expect(formatPrice(10.99, "USD")).to.equal("$10.99");
+        expect(formatPrice(10.99, "EUR")).to.equal("€10.99");
+
+        Object.defineProperty(navigator, "language", {
+            value: "de-DE",
+            configurable: true
+        });
+
+        expect(formatPrice(10.99, "USD")).to.equal("10,99 $");
+        expect(formatPrice(10.99, "EUR")).to.equal("10,99 €");
+
+        cy.window().then(win => {
+            Object.defineProperty(win, "navigator", {
+                value: undefined
+            });
+
+            expect(formatPrice(10.99, "USD")).to.equal("");
+        })
     })
 })
