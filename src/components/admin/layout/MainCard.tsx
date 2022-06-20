2ba2a35a1f2748def46937828ede254bcfbf2523
Marius Butz <mbpictures13@gmail.com>
2022-06-20 10:20:15 +0200

    increase elevation of main card

--- src/components/admin/layout/MainCard.tsx
+++ src/components/admin/layout/MainCard.tsx
@@ -40,7 +40,7 @@ const CardWrapper = styled(Card)<{color: PaletteColor}>(({theme, color}: {theme:
 
 export const MainCard = ({title, secondaryTitle, titleIcon, icon, color}: {title?: JSX.Element | string, secondaryTitle?: JSX.Element | string, titleIcon?: JSX.Element, icon?: JSX.Element, color?: PaletteColor}) => {
     return (
-        <CardWrapper color={color}>
+        <CardWrapper color={color} elevation={3}>
             <Box sx={{ p: 2.25 }}>
                 <Grid container direction="column">
                     <Grid item>
