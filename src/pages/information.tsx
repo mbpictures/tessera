6a1850c05bd19fbee08148f367e50a1fef9c252d
Marius Butz <mbpictures13@gmail.com>
2022-09-11 00:47:57 +0200

    add ticket name entering to information page

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -1,5 +1,13 @@
 import { Step } from "../components/Step";
-import { Card, Stack, TextField, Typography, useMediaQuery } from "@mui/material";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary, Button,
+    Stack,
+    TextField,
+    Typography,
+    useMediaQuery
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { CheckboxAccordion } from "../components/CheckboxAccordion";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
@@ -16,6 +24,8 @@ import { getOption } from "../lib/options";
 import { Options } from "../constants/Constants";
 import useTranslation from "next-translate/useTranslation";
 import loadNamespaces from "next-translate/loadNamespaces";
+import { TicketNames } from "../components/form/TicketNames";
+import prisma from "../lib/prisma";
 
 const validateEmail = (email) => {
     const re =
@@ -23,10 +33,11 @@ const validateEmail = (email) => {
     return re.test(String(email).toLowerCase());
 };
 
-export default function Information({ direction, deliveryMethods }) {
+export default function Information({ direction, deliveryMethods, categories }) {
     const selector = useAppSelector(selectPersonalInformation);
     const dispatch = useAppDispatch();
     const { t } = useTranslation();
+    const [expanded, setExpanded] = useState(0);
 
     const [selectedShippingMethod, setSelectedShippingMethod] =
         useState<ShippingType | null>(selector.shipping?.type ?? null);
@@ -61,6 +72,11 @@ export default function Information({ direction, deliveryMethods }) {
         );
     }, [selectedShippingMethod, dispatch]);
 
+    const handleAccordionChange = (index) => (event, isExpanded) => {
+        if (!isExpanded) return;
+        setExpanded(index);
+    }
+
     return (
         <Step
             direction={direction}
@@ -72,50 +88,65 @@ export default function Information({ direction, deliveryMethods }) {
             }}
         >
             <Box style={boxStyling} sx={{ py: 2 }}>
-                <Card>
-                    <Stack padding={1} spacing={1}>
-                        <Typography>
-                            {t("information:address-for-invoice")}
-                        </Typography>
-                        <TextField
-                            label={t("information:e-mail")}
-                            type="email"
-                            value={selector.email}
-                            onChange={(event) =>
-                                dispatch(setEmail(event.target.value))
-                            }
-                            error={emailError != null}
-                            helperText={emailTouched && emailError}
-                            name={"address-email"}
-                            onBlur={() => setEmailTouched(true)}
-                        />
-                        <AddressComponent
-                            value={selector.address}
-                            onChange={(newValue) =>
-                                dispatch(setAddress(newValue))
-                            }
-                        />
-                    </Stack>
-                </Card>
-                {
-                    // iterate through enum and filter to keep order constant
-                    Object.values(ShippingType)
-                        .filter((type) => deliveryMethods.includes(type))
-                        .map((shippingType) => {
-                            const instance = ShippingFactory.getShippingInstance({type: shippingType, data: null});
-                            return (
-                                <CheckboxAccordion
-                                    label={t(`information:${instance.DisplayName}`)}
-                                    name={shippingType}
-                                    selectedItem={selectedShippingMethod}
-                                    onSelect={setSelectedShippingMethod}
-                                    key={shippingType}
-                                >
-                                    {instance.Component}
-                                </CheckboxAccordion>
-                            )
-                        })
-                }
+                <Accordion expanded={expanded === 0} onChange={handleAccordionChange(0)}>
+                    <AccordionSummary>{t("information:address")}</AccordionSummary>
+                    <AccordionDetails>
+                        <Stack padding={1} spacing={1}>
+                            <Typography>
+                                {t("information:address-for-invoice")}
+                            </Typography>
+                            <TextField
+                                label={t("information:e-mail")}
+                                type="email"
+                                value={selector.email}
+                                onChange={(event) =>
+                                    dispatch(setEmail(event.target.value))
+                                }
+                                error={emailError != null}
+                                helperText={emailTouched && emailError}
+                                name={"address-email"}
+                                onBlur={() => setEmailTouched(true)}
+                            />
+                            <AddressComponent
+                                value={selector.address}
+                                onChange={(newValue) =>
+                                    dispatch(setAddress(newValue))
+                                }
+                            />
+                            <Button onClick={() => setExpanded(1)} fullWidth={true}>Next</Button>
+                        </Stack>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expanded === 1} onChange={handleAccordionChange(1)}>
+                    <AccordionSummary>{t("information:tickets")}</AccordionSummary>
+                    <AccordionDetails>
+                        <TicketNames categories={categories} />
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expanded === 2} onChange={handleAccordionChange(2)}>
+                    <AccordionSummary>{t("information:delivery")}</AccordionSummary>
+                    <AccordionDetails>
+                        {
+                            // iterate through enum and filter to keep order constant
+                            Object.values(ShippingType)
+                                .filter((type) => deliveryMethods.includes(type))
+                                .map((shippingType) => {
+                                    const instance = ShippingFactory.getShippingInstance({type: shippingType, data: null});
+                                    return (
+                                        <CheckboxAccordion
+                                            label={t(`information:${instance.DisplayName}`)}
+                                            name={shippingType}
+                                            selectedItem={selectedShippingMethod}
+                                            onSelect={setSelectedShippingMethod}
+                                            key={shippingType}
+                                        >
+                                            {instance.Component}
+                                        </CheckboxAccordion>
+                                    )
+                                })
+                        }
+                    </AccordionDetails>
+                </Accordion>
             </Box>
         </Step>
     );
@@ -126,6 +157,7 @@ export async function getStaticProps({ locale }) {
     return {
         props: {
             deliveryMethods,
+            categories: await prisma.category.findMany(),
             theme: await getOption(Options.Theme),
             ...(await loadNamespaces({ locale, pathname: '/information' }))
         }
--- src/store/reducers/orderReducer.tsx
+++ src/store/reducers/orderReducer.tsx
@@ -10,6 +10,8 @@ export interface Ticket {
     seatId?: number;
     categoryId: number;
     amount: number;
+    firstName?: string;
+    lastName?: string;
 }
 
 export type Tickets = Array<Ticket>;
@@ -28,10 +30,16 @@ export const orderSlice = createSlice({
         setOrderId: (state, action: PayloadAction<string>) => {
             state.orderId = action.payload;
         },
+        setTicketFirstName: (state, action: PayloadAction<{index: number; firstName: string;}>) => {
+            state.tickets[action.payload.index].firstName = action.payload.firstName;
+        },
+        setTicketLastName: (state, action: PayloadAction<{index: number; lastName: string;}>) => {
+            state.tickets[action.payload.index].lastName = action.payload.lastName;
+        },
         resetOrder: () => initialState
     }
 });
 
-export const { setTickets, setOrderId, resetOrder } = orderSlice.actions;
+export const { setTickets, setOrderId, resetOrder, setTicketFirstName, setTicketLastName } = orderSlice.actions;
 export const selectOrder = (state: RootState) => state.order;
 export default orderSlice.reducer;
