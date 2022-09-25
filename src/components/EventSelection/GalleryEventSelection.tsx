126f55fb9a45a41b916b0b722b6b2049f6c6f4e9
Marius Butz <mbpictures13@gmail.com>
2022-09-25 17:28:05 +0200

    fix gallery event selection date/event not pre-selected after navigating back again

--- src/components/EventSelection/GalleryEventSelection.tsx
+++ src/components/EventSelection/GalleryEventSelection.tsx
@@ -1,20 +1,27 @@
 import style from "../../style/GalleryEventSelection.module.scss";
 import { GalleryEventSelectionEntry } from "./GalleryEventSelectionEntry";
 import { useEffect, useState } from "react";
+import { useAppSelector } from "../../store/hooks";
+import { selectEventSelected } from "../../store/reducers/eventSelectionReducer";
 
 export const GalleryEventSelection = ({events, onChange}) => {
+    const currentSelectedDate = useAppSelector(selectEventSelected);
     const [currentEvent, setCurrentEvent] = useState(null);
 
     const needsDateSelection = (event) => event && event.dates.length > 1;
 
     useEffect(() => {
-        if (!currentEvent || needsDateSelection(currentEvent)) {
-            onChange(-1);
-            return;
-        }
-        onChange(currentEvent.dates[0].id);
-    }, [currentEvent]);
+        if (currentSelectedDate === -1) return;
+        setCurrentEvent(events.find(event => event.dates.some(date => date.id === currentSelectedDate)));
+    }, []);
 
+    const handleChange = (event) => {
+        setCurrentEvent(event);
+        if (!event || needsDateSelection(event))
+            onChange(-1);
+        else
+            onChange(event.dates[0].id);
+    }
 
     return (
         <div style={{
@@ -26,7 +33,7 @@ export const GalleryEventSelection = ({events, onChange}) => {
                         <GalleryEventSelectionEntry
                             key={index}
                             event={event}
-                            onChange={setCurrentEvent}
+                            onChange={handleChange}
                             selected={currentEvent?.id === event.id}
                             onDateChange={onChange}
                         />
