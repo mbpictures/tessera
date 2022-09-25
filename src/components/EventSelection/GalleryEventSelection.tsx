7567a920ab20b3d1e7e9fcb5ae3f63dbc9e293e3
Marius Butz <mbpictures13@gmail.com>
2022-09-25 17:03:24 +0200

    implement multiple date support in gallery event selection

--- src/components/EventSelection/GalleryEventSelection.tsx
+++ src/components/EventSelection/GalleryEventSelection.tsx
@@ -1,12 +1,38 @@
 import style from "../../style/GalleryEventSelection.module.scss";
 import { GalleryEventSelectionEntry } from "./GalleryEventSelectionEntry";
+import { useEffect, useState } from "react";
 
 export const GalleryEventSelection = ({events, onChange}) => {
+    const [currentEvent, setCurrentEvent] = useState(null);
+
+    const needsDateSelection = (event) => event && event.dates.length > 1;
+
+    useEffect(() => {
+        if (!currentEvent || needsDateSelection(currentEvent)) {
+            onChange(-1);
+            return;
+        }
+        onChange(currentEvent.dates[0].id);
+    }, [currentEvent]);
+
+
     return (
-        <div className={style.eventSelectionGallery}>
-            {
-                events.map((event, index) => <GalleryEventSelectionEntry key={index} event={event} index={event.id} onChange={onChange} />)
-            }
+        <div style={{
+            position: "relative"
+        }}>
+            <div className={style.eventSelectionGallery}>
+                {
+                    events.map((event, index) => (
+                        <GalleryEventSelectionEntry
+                            key={index}
+                            event={event}
+                            onChange={setCurrentEvent}
+                            selected={currentEvent?.id === event.id}
+                            onDateChange={onChange}
+                        />
+                    ))
+                }
+            </div>
         </div>
     );
 }
