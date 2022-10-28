ac4c7a90557e92583903a9c543669a3d8bc6b151
Marius Butz <mbpictures13@gmail.com>
2022-10-28 16:33:56 +0200

    add api route to load gtc

---
+++ src/pages/api/gtc.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Options } from "../../constants/Constants";
+import { getOptionData } from "../../lib/options";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "GET")
+        return res.status(400).end("Method unsupported");
+
+    const { data, type } = await getOptionData(Options.GTC);
+    if (!data)
+        return res.status(404).end("No GTC set");
+
+    res.setHeader("Cache-Control", "s-maxage=86400");
+    res.setHeader("Content-Type", type);
+    res.status(200).send(data);
+}
